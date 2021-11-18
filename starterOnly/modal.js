e0013630d6ffca5627dd474efc85a21ae89e2ae4
LoopDavid <dpereira@outlook.fr>
2021-11-18 21:42:55 +0100

    validation form ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,7 +21,6 @@ const emailError = document.getElementById('email-error-msg');
 const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const birthdateFormat = /^\d{2}-\d{2}-\d{4}$/;
 const birthdate = document.getElementById('birthdate');
-<<<<<<< HEAD
 const birthdateError = document.getElementById('birthdate-error-msg');
 const quantity = document.getElementById('quantity');
 const quantityError = document.getElementById('quantity-error-msg');
@@ -30,12 +29,8 @@ const locError = document.getElementById('loc-error-msg');
 let locCheck = false;
 const rgpd = document.getElementById('checkbox1');
 const rgpdError = document.getElementById('rgpd-error-msg');
-let rgpdCheck = false;
 const sendMsg = document.getElementById('send-msg');
-=======
-const birthdateError = document.getElementById('birthdate-msg-error');
-
->>>>>>> 131a189cce9923a220939bcbcede616761138c25
+const content = document.getElementsByClassName('content');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -50,7 +45,6 @@ function launchModal() {
 // stop to close modal in submit modalBtn
 document.getElementById('myForm').addEventListener('submit', function(e) {
 e.preventDefault();
-<<<<<<< HEAD
   if (prenom.value.length >= 2) {
     prenomError.innerHTML="";
     prenom.style.border = "2px solid green";
@@ -106,11 +100,14 @@ e.preventDefault();
     locError.innerHTML="Vous devez choisir une option.";
     return;
   }
-  console.log(rgpd.checked);
 
-
-
-
+  if (rgpd.checked == true) {
+    rgpdError.innerHTML="";
+  } else {
+    rgpdError.innerHTML="false";
+    return;
+  } 
+    
   formElement.style.display='none';
   sendMsg.style.display='block';
   formElement.reset();
@@ -121,7 +118,6 @@ const submitBtnElement = document.getElementById('submit-btn');
 const formElement = document.getElementById('myForm');
 
 submitBtnElement.addEventListener('click', () => {
-  
 });
 
 // close modal
@@ -130,42 +126,4 @@ close.addEventListener('click', () => {
   formElement.style.display='block';
   sendMsg.style.display='none';
   formElement.reset();
-=======
-if (prenom.value.length <= 2) {
-  prenomError.innerHTML="Veuillez mettre 3 caractères minimun";
-  prenom.style.border = "2px solid red";
-  return;
-  } else {
-    prenomError.innerHTML="";
-    prenom.style.border = "2px solid green";
-
-  }
-  if (nom.value.length <= 2) {
-    nomError.innerHTML="Veuillez entrer 3 caractères au minimun !";
-    nom.style.border = "2px solid red";
-    return;
-  } else {
-    nomError.innerHTML="";
-    nom.style.border = "2px solid green";
-  }
-  if (email.value.match(emailFormat)) {
-    return true;
-  } else {
-    emailError.innerHTML="Veuillez entrer une adresse mail valide !";
-    email.style.border = "2px solid red";
-    console.log('email invalid');
-  }
-  // if (birthdate.value.match(birdthdateFormat)) {
-  //   return true;
-  // } else {
-  //   birthdateError.innerHTML="Veuillez une date au format suivant jj/mm/aaaa";
-  //   birthdate.style.border ="2px solid red";
-  // }
-});
-
-// close modal
-
-close.addEventListener('click', () => {
-  modalbg.style.display = "none";
->>>>>>> 131a189cce9923a220939bcbcede616761138c25
 });

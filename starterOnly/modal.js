c8a3016ec99c1455e569c9fa9949a3c5e3848dc4
LoopDavid <dpereira@outlook.fr>
2021-11-18 10:34:30 +0100

    imput msg error add

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,18 +21,15 @@ const emailError = document.getElementById('email-error-msg');
 const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const birthdateFormat = /^\d{2}-\d{2}-\d{4}$/;
 const birthdate = document.getElementById('birthdate');
-const birthdateError = document.getElementById('birthdate-msg-error');
+const birthdateError = document.getElementById('birthdate-error-msg');
 const quantity = document.getElementById('quantity');
 const quantityError = document.getElementById('quantity-error-msg');
-const loc = document.getElementById('loc').checked;
+const loc = document.querySelectorAll('input[name="location"]');
 const locError = document.getElementById('loc-error-msg');
-// const loc1 = document.getElementById('location1');
-// const loc2 = document.getElementById('location2');
-// const loc3 = document.getElementById('location3');
-// const loc4 = document.getElementById('location4');
-// const loc5 = document.getElementById('location5');
-// const loc6 = document.getElementById('location6');
-const locFormat = /^[0-9]$/;
+let locCheck = false;
+const rgpd = document.getElementById('rgpd');
+const rgpdError = document.getElementById('rgpd-error-msg');
+let rgpdCheck = false;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -46,48 +43,76 @@ function launchModal() {
 // stop to close modal in submit modalBtn
 document.getElementById('myForm').addEventListener('submit', function(e) {
 e.preventDefault();
-if (prenom.value.length <= 2) {
-  prenomError.innerHTML="Veuillez mettre 3 caractères minimun";
-  prenom.style.border = "2px solid red";
-  return;
-  } else {
+  if (prenom.value.length >= 2) {
     prenomError.innerHTML="";
     prenom.style.border = "2px solid green";
-
-  }
-  if (nom.value.length <= 2) {
-    nomError.innerHTML="Veuillez entrer 3 caractères au minimun !";
-    nom.style.border = "2px solid red";
-    return;
   } else {
+    prenomError.innerHTML="Veuillez mettre 2 caractères minimun";
+    prenom.style.border = "2px solid red";
+  }
+
+  if (nom.value.length >= 2) {
     nomError.innerHTML="";
     nom.style.border = "2px solid green";
+  } else {
+    nomError.innerHTML="Veuillez entrer 2 caractères au minimun !";
+    nom.style.border = "2px solid red";
+    
   }
+
   if (email.value.match(emailFormat)) {
-    email.style.border = "2px solid green";
-    return true;
+    email.style.border="2px solid green";
+    emailError.innerHTML="";
   } else {
     emailError.innerHTML="Veuillez entrer une adresse mail valide !";
     email.style.border = "2px solid red";
     console.log('email invalid');
   }
-  if (birthdate.value.match(birthdateFormat)) {
+
+  if (birthdate.value.length > 0) {
     birthdate.style.border="2px solid green";
-    return true;
+    birthdateError.innerHTML="";
   } else {
-    birthdateError.innerHTML="Champ obligatoire !";
+    birthdateError.innerHTML="Veuillez une date au format suivant jj/mm/aaaa";
     birthdate.style.border ="2px solid red";
   }
-  if (quantity.value >= 0 || quantity.value <= 99 || loc.value.match(locFormat)) {
-    return true;
+  
+  if (quantity.value.length >= 1 && quantity.value >= 0 && quantity.value <= 99) {
+    quantity.style.border="2px solid green";
+    quantityError.innerHTML="";
   } else {
-    quantityError.innerHTML ="Veuillez composer des chiffre de 0 à 9 et entre 0 et 99.";
+    quantityError.innerHTML="Veuillez entrer un nombre entre 0 et 99 !";
     quantity.style.border ="2px solid red";
   }
+  
+  for (let i = 0; i < loc.length; i++) {
+  locCheck = (loc[i].checked || locCheck) ;
+  }
+  if (locCheck) {
+    locError.innerHTML="";
+  } else {
+    locError.innerHTML="Vous devez choisir une option.";
+  }
+  
+  for (let i = 0; i < rgpd.length; i++) {
+    locCheck = (rgpd[i].checked || rgpdCheck) ;
+    }
+    if (locCheck) {
+      rgpdError.innerHTML="";
+    } else {
+      rgpdError.innerHTML="Vous devez vérifier que vous acceptez les termes et conditions.";
+    }
 });
+// Validate Form 
+
+
+function validate() {
+
+};
+
+
 
 // close modal
-
 close.addEventListener('click', () => {
   modalbg.style.display = "none";
 });
